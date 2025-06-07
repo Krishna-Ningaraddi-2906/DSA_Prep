156e999169ecceaf67c3257dbf4174b5f47e30b7
Krishna Ningaraddi <krishnaningaraddi2906@gmail.com>
2025-06-07 11:09:26 +0530

    third Program

--- Twosum.java
+++ Twosum.java
@@ -1,7 +1,33 @@
-public class TwoSum 
+import java.util.*;
+public class Twosum 
 {
-    public static void main(String[] args) {
-        
-        
+    public static void main(String[] args) 
+    {
+        int[] arr= {1,2,4,5,3,5,1};
+        int target=10;
+
+        int[] sol=twoSum(arr, target);
+
+        System.out.println(Arrays.toString(sol));
+
     }
-}
+
+ 
+       public static int[] twoSum(int[] nums, int target) 
+       {
+       HashMap<Integer, Integer> map= new HashMap<>();
+
+       for(int i=0;i<nums.length;i++)
+       {
+        int ref = target-nums[i];
+
+        if(map.containsKey(ref))
+            return new int[]{map.get(ref),i};
+        map.put(nums[i],i);
+       }
+
+       return new int[]{};
+    }
+
+    
+    }
